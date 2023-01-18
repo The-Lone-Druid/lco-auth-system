a3c1408afa8af4e067c04e8da51661de45fd22ae
Zahid Shaikh <zahid.786shaikh@gmail.com>
2023-01-18 09:52:06 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello from auth system - LCO</h1>");
+});
+
+module.exports = app;
---
+++ index.js
@@ -0,0 +1,5 @@
+const app = require("./app");
+
+app.listen(8080, () => {
+  console.log(`Server is running at port http://localhost:4000`);
+});
